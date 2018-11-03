7a0ac75e631403c000576d85401c44edc76dc8e8
michaelstsnow <michael.st.snow@gmail.com>
2018-11-03 01:03:09 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(collection)
+  i=0;
+  final_collection=[];
+  while i<collection.size()
+    item=collection[i];
+    yield(item)
+    final_collection << yield(item);
+    i+=1;
+  end
+  final_collection
+end
